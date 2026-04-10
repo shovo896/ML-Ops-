5a7e8f668e7f6132e1f78a5d0534076bf4edd92c
Shuvo <suvo10.info@gmail.com>
2026-04-10 10:31:51 +0600

    gjmnvl

--- mlflow/demo.py
+++ mlflow/demo.py
@@ -41,6 +41,26 @@ alpha=float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
 l1_ratio=float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
 
+with mlflow.start_run():
+    lr=ElasticNet(alpha=alpha,l1_ratio=l1_ratio,random_state=42)
+    lr.fit(train_x,train_y)
+
+    predicted_qualities=lr.predict(test_x)
+    (rmse,mae,r2)=eval_metrics(test_y,predicted_qualities)
+    print("Elasticnet model (alpha=%f,l1_ratio=%f):" % (alpha,l1_ratio))    
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
+    
+    remote_server_uri = 
+
+
 
 
 
